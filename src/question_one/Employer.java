8225de3ddb842e8e974be5d3b4f117f9ef15f86a
Lucas Godoy <pedrolucf@gmail.com>
2023-05-10 15:10:40 -0300

    Tratamento de erros, exceptions e formatação

--- src/question_one/Employer.java
+++ src/question_one/Employer.java
@@ -2,7 +2,6 @@ package question_one;
 
 import java.text.DecimalFormat;
 
-
 public class Employer {
   private String name;
   private double salary;
@@ -14,12 +13,12 @@ public class Employer {
   private String bonusFormated;
 
   DecimalFormat df = new DecimalFormat("#,##0.00");
-  
+
   /**
    * @param name
    * @param salary
    */
-  public Employer(String name, double salary){
+  public Employer(String name, double salary) {
 
     this.name = name;
     this.salary = salary;
@@ -28,145 +27,144 @@ public class Employer {
     setBonusAndDiscount(salary);
     setTextIsBonusOrDiscount(textIsBonusOrDiscount);
   }
-    
 
-    /**
-     * @return String return the name
-     */
-    public String getName() {
-        return name;
-    }
+  /**
+   * @return String return the name
+   */
+  public String getName() {
+    return name;
+  }
 
-    /**
-     * @param name the name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
+  /**
+   * @param name the name to set
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    /**
-     * @return double return the salary
-     */
-    public double getSalary() {
-        return salary;
-    }
+  /**
+   * @return double return the salary
+   */
+  public double getSalary() {
+    return salary;
+  }
 
-    /**
-     * @param salary the salary to set
-     */
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
+  /**
+   * @param salary the salary to set
+   */
+  public void setSalary(double salary) {
+    this.salary = salary;
+  }
 
-    /**
-     * @return double return the liquidSalary
-     */
-    public double getLiquidSalary() {
-        return liquidSalary;
-    }
+  /**
+   * @return double return the liquidSalary
+   */
+  public double getLiquidSalary() {
+    return liquidSalary;
+  }
 
-    /**
-     * @param liquidSalary the liquidSalary to set
-     */
-    public void setLiquidSalary(double liquidSalary) {
-        //Liquid Salary
-    if(this.salary <= 1000){
+  /**
+   * @param liquidSalary the liquidSalary to set
+   */
+  public void setLiquidSalary(double liquidSalary) {
+    // Liquid Salary
+    if (this.salary <= 1000) {
       this.liquidSalary = this.salary + (this.salary * 0.2);
-    }else if(this.salary > 1000 && this.salary < 2000){
-       this.liquidSalary = this.salary + (this.salary * 0.1);
-    }else if(this.salary >= 2000){
+    } else if (this.salary > 1000 && this.salary < 2000) {
+      this.liquidSalary = this.salary + (this.salary * 0.1);
+    } else if (this.salary >= 2000) {
       this.liquidSalary = this.salary - (this.salary * 0.1);
     }
-    }
+  }
 
-    /**
-     * @return double return the bonusAndDiscount
-     */
-    public double getBonusAndDiscount() {
-        return bonusAndDiscount;
-    }
+  /**
+   * @return double return the bonusAndDiscount
+   */
+  public double getBonusAndDiscount() {
+    return bonusAndDiscount;
+  }
 
-    /**
-     * @param bonusAndDiscount the bonusAndDiscount to set
-     */
-    public void setBonusAndDiscount(double bonusAndDiscount) {
-        //Bonus And Discount
-    if(this.salary <= 1000){
+  /**
+   * @param bonusAndDiscount the bonusAndDiscount to set
+   */
+  public void setBonusAndDiscount(double bonusAndDiscount) {
+    // Bonus And Discount
+    if (this.salary <= 1000) {
       this.bonusAndDiscount = this.salary * 0.2;
-    }else if(this.salary > 1000 && this.salary < 2000){
-       this.bonusAndDiscount = this.salary * 0.1;
-    }else if(this.salary >= 2000){
+    } else if (this.salary > 1000 && this.salary < 2000) {
+      this.bonusAndDiscount = this.salary * 0.1;
+    } else if (this.salary >= 2000) {
       this.bonusAndDiscount = this.salary * 0.1;
     }
-    }
+  }
 
-    /**
-     * @return String return the textIsBonusOrDiscount
-     */
-    public String getTextIsBonusOrDiscount() {
-        return textIsBonusOrDiscount;
-    }
+  /**
+   * @return String return the textIsBonusOrDiscount
+   */
+  public String getTextIsBonusOrDiscount() {
+    return textIsBonusOrDiscount;
+  }
 
-    /**
-     * @param textIsBonusOrDiscount the textIsBonusOrDiscount to set
-     */
-    public void setTextIsBonusOrDiscount(String textIsBonusOrDiscount) {
-            //Bonus And Discount
-    if(this.salary <= 1000){
-      this.textIsBonusOrDiscount = "Bonus: +R$"; 
-    }else if(this.salary > 1000 && this.salary < 2000){
+  /**
+   * @param textIsBonusOrDiscount the textIsBonusOrDiscount to set
+   */
+  public void setTextIsBonusOrDiscount(String textIsBonusOrDiscount) {
+    // Bonus And Discount
+    if (this.salary <= 1000) {
       this.textIsBonusOrDiscount = "Bonus: +R$";
-    }else if(this.salary >= 2000){
+    } else if (this.salary > 1000 && this.salary < 2000) {
+      this.textIsBonusOrDiscount = "Bonus: +R$";
+    } else if (this.salary >= 2000) {
       this.textIsBonusOrDiscount = "Discount: -R$";
     }
-    }
+  }
 
-    /**
-     * @return String return the salaryFormated
-     */
-    public String getSalaryFormated() {
-        return salaryFormated;
-    }
+  /**
+   * @return String return the salaryFormated
+   */
+  public String getSalaryFormated() {
+    return salaryFormated;
+  }
 
-    /**
-     * @param salaryFormated the salaryFormated to set
-     */
-    public void setSalaryFormated(String salaryFormated) {
-        this.salaryFormated = salaryFormated;
-    }
+  /**
+   * @param salaryFormated the salaryFormated to set
+   */
+  public void setSalaryFormated(String salaryFormated) {
+    this.salaryFormated = salaryFormated;
+  }
 
-    /**
-     * @return String return the liquidSalaryFormated
-     */
-    public String getLiquidSalaryFormated() {
-        return liquidSalaryFormated;
-    }
+  /**
+   * @return String return the liquidSalaryFormated
+   */
+  public String getLiquidSalaryFormated() {
+    return liquidSalaryFormated;
+  }
 
-    /**
-     * @param liquidSalaryFormated the liquidSalaryFormated to set
-     */
-    public void setLiquidSalaryFormated(String liquidSalaryFormated) {
-        this.liquidSalaryFormated = df.format(Double.toString(this.liquidSalary));
-    }
+  /**
+   * @param liquidSalaryFormated the liquidSalaryFormated to set
+   */
+  public void setLiquidSalaryFormated(String liquidSalaryFormated) {
+    this.liquidSalaryFormated = df.format(Double.toString(this.liquidSalary));
+  }
 
-    /**
-     * @return String return the bonusFormated
-     */
-    public String getBonusFormated() {
-        return bonusFormated;
-    }
+  /**
+   * @return String return the bonusFormated
+   */
+  public String getBonusFormated() {
+    return bonusFormated;
+  }
 
-    /**
-     * @param bonusFormated the bonusFormated to set
-     */
-    public void setBonusFormated(String bonusFormated) {
-        this.bonusFormated = bonusFormated;
-    }
+  /**
+   * @param bonusFormated the bonusFormated to set
+   */
+  public void setBonusFormated(String bonusFormated) {
+    this.bonusFormated = bonusFormated;
+  }
 
-
-    @Override
-    public String toString() {
-        return "\n" + "Employer: " + name + "\n" + "Salary: R$" + df.format(salary) + "\n" + this.textIsBonusOrDiscount + df.format(this.bonusAndDiscount) + "\n"
+  @Override
+  public String toString() {
+    return "\n" + "Employer: " + name + "\n" + "Salary: R$" + df.format(salary) + "\n" + this.textIsBonusOrDiscount
+        + df.format(this.bonusAndDiscount) + "\n"
         + "Liquid Salary: R$" + df.format(this.liquidSalary);
-    }
+  }
 }
--- src/question_one/EmployerRegister.java
+++ src/question_one/EmployerRegister.java
@@ -1,60 +1,82 @@
 package question_one;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class EmployerRegister {
-  
+
   private ArrayList<Employer> employers;
 
-  public EmployerRegister(){
+  public EmployerRegister() {
     employers = new ArrayList<>();
   }
 
   public void employerRegister() {
     Scanner rd = new Scanner(System.in);
-  
-    System.out.println("How many employees do you want to register? : ");
-    int n = rd.nextInt();
-  
-    for(int i = 0; i < n; i++){
-      System.out.println("Employer #"+ (i + 1));
-      System.out.println("Employer Name : ");
-      String name = rd.next();
-      System.out.println("Employer Salary : ");
-      double salary = rd.nextDouble();
-  
-      Employer employer = new Employer(name, salary);
-      employers.add(employer);
+
+    try {
+      System.out.println("How many employees do you want to register? : ");
+      int n = rd.nextInt();
+
+      if (n < 0) {
+        throw new IllegalArgumentException("The number of employees cannot be negative.");
+      }
+
+      for (int i = 0; i < n; i++) {
+        System.out.println("Employer #" + (i + 1));
+        System.out.println("Employer Name : ");
+        String name = rd.next();
+        if (name.isEmpty()) {
+          throw new IllegalArgumentException("Employer name cannot be empty.");
+        } else if (name.matches(".*\\d+.*") || name.matches(".*[!@#$%^&*()\\-_=+\\\\|\\[{\\]};:'\",<.>/?].*")) {
+          throw new IllegalArgumentException(
+              "Employer name can't have invalid caracters as: 'numbers and especial caracters'.");
+        }
+
+        System.out.println("Employer Salary : ");
+        double salary = rd.nextDouble();
+        if (salary < 0) {
+          throw new IllegalArgumentException("Employer salary can't be a negative value.");
+        }
+
+        Employer employer = new Employer(name, salary);
+        employers.add(employer);
+      }
+
+    } catch (InputMismatchException e) {
+      System.out.println("Invalid Input");
+      System.out.println("Restarting...");
+      employerRegister();
+    } catch (IllegalArgumentException er) {
+      System.out.println(er.getMessage());
+      System.out.println("Restarting...");
+      employerRegister();
+    } finally {
+      rd.close();
     }
-    rd.close();
   }
 
-  public void showEmployersData(){
+  public void showEmployersData() {
     System.out.println("");
     System.out.println("Employers: ");
-    for (Employer employer : employers){
+    for (Employer employer : employers) {
       System.out.println(employer);
     }
   }
 
+  /**
+   * @return ArrayList<Employer> return the employers
+   */
+  public ArrayList<Employer> getEmployers() {
+    return employers;
+  }
 
-
-
-
-    /**
-     * @return ArrayList<Employer> return the employers
-     */
-    public ArrayList<Employer> getEmployers() {
-        return employers;
-    }
-
-    /**
-     * @param employers the employers to set
-     */
-    public void setEmployers(ArrayList<Employer> employers) {
-        this.employers = employers;
-    }
+  /**
+   * @param employers the employers to set
+   */
+  public void setEmployers(ArrayList<Employer> employers) {
+    this.employers = employers;
+  }
 
 }
-
