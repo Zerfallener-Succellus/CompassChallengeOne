7011b7946098d96040f5f10ca7c531700d9d66aa
Lucas Godoy <pedrolucf@gmail.com>
2023-05-30 23:59:09 -0300

    melhoras finais apos feedback da q2

--- src/question_two/StringReciver.java
+++ src/question_two/StringReciver.java
@@ -1,5 +1,6 @@
 package question_two;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,41 +10,78 @@ public class StringReciver {
   private String exit;
 
   public void stringReciver() {
+    try {
+      boolean isRunning = true;
 
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter your mensage: ");
-    String m = sc.nextLine();
-    while (m.isEmpty()) {
-      System.out.println("Please enter a valid message: ");
-      m = sc.nextLine();
-    }
-    sc.close();
+      while (isRunning) {
 
-    Pattern emotes = Pattern.compile(":-\\)|:-\\(");
-    Matcher matcher = emotes.matcher(m);
+        System.out.println("Do you want to enter a text to count the emoticons ?");
+        System.out.println("1. YES (CONTINUE)");
+        System.out.println("2. NO (CLOSE)");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(">>> ");
+        int m = sc.nextInt();
 
-    int funEmote = 0;
-    int upsetEmote = 0;
+        switch (m) {
+          case 1 ->
+            stringFormatter(stringReceive());
 
-    while (matcher.find()) {
-      if (matcher.group().equals(":-)")) {
-        funEmote++;
-      } else if (matcher.group().equals(":-(")) {
-        upsetEmote++;
+          case 2 -> {
+            System.out.println("CLOSING PROGRAM");
+            isRunning = false;
+          }
+        }
+
+
       }
+    } catch (InputMismatchException e) {
+      System.out.println("Invalid Input");
+      System.out.println("Restarting...");
+      stringReciver();
     }
-
-    if (funEmote == upsetEmote) {
-      this.exit = "Neutral";
-    } else if (funEmote > upsetEmote) {
-      this.exit = "Fun";
-    } else if (funEmote < upsetEmote) {
-      this.exit = "Upset";
-    }
-
-    System.out.println(toString());
-
   }
+
+    public String stringReceive () {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter your message: ");
+      String m = sc.nextLine();
+      while (m.isEmpty()) {
+        System.out.println("Please enter a valid message: ");
+        m = sc.nextLine();
+      }
+      return m;
+    }
+
+    public void stringFormatter (String texto){
+
+
+      Pattern emotes = Pattern.compile(":-\\)|:-\\(");
+      Matcher matcher = emotes.matcher(texto);
+
+      int funEmote = 0;
+      int upsetEmote = 0;
+
+      while (matcher.find()) {
+        if (matcher.group().equals(":-)")) {
+          funEmote++;
+        } else if (matcher.group().equals(":-(")) {
+          upsetEmote++;
+        }
+      }
+
+      if (funEmote == upsetEmote) {
+        this.exit = "Neutral";
+      } else if (funEmote > upsetEmote) {
+        this.exit = "Fun";
+      } else if (funEmote < upsetEmote) {
+        this.exit = "Upset";
+      }
+
+      System.out.println(toString());
+    }
+
+
+
 
   @Override
   public String toString() {
